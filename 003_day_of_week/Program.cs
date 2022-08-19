b0fbbe106441effc4b613509e94ae3fcccd9e7f4
alex <shura888@inbox.ru>
2022-08-19 18:07:31 +0300

    task_3

---
+++ 003_day_of_week/Program.cs
@@ -0,0 +1,42 @@
+﻿// 3. Напишите программу, которая будет выдавать
+// название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+// Вариант преподавателя 1 и 2
+
+Console.Write("Enter number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 1 && number <= 7 )
+{
+
+switch(number)
+{
+case 1: System.Console.WriteLine("Monday"); break;
+
+case 2: System.Console.WriteLine("Tuesday"); break;
+
+case 3: System.Console.WriteLine("Wednesday"); break;
+
+default: System.Console.WriteLine("Bad number");  break;
+}
+
+if(number == 1){
+System.Console.WriteLine("Monday");
+}  
+else if(number == 2){
+System.Console.WriteLine("Tuesday");
+}
+else if(number == 3){
+System.Console.WriteLine("Wednesday");
+}
+else{
+System.Console.WriteLine("Bad number"); 
+}
+
+}
+else
+{
+System.Console.WriteLine("Bad Number");
+}
