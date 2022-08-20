a8e869f7cfed8ffb397b5c7545f0e6f4018fd4c0
alex <shura888@inbox.ru>
2022-08-20 23:26:18 +0300

    010

---
+++ 010_MethodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿
+int [] array = {15,2,53,64,554,66,97,78,29};
+
+int n = array.Length;
+int find = 78;
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+    }
+    index++;
+}
