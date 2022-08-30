339811bfa84f67c24bb0072910de8796d18d8061
alex <shura888@inbox.ru>
2022-08-30 16:37:23 +0300

    Home_Seminar_3

---
+++ home_Seminar_3/Home_19/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+using System;
+
+namespace Home3
+{
+    class TestMetods
+    {
+        public static void Main(string[] args)
+        {
+            // Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+            // 14212 -> нет
+            // 23432 -> да
+            // 12821 -> да
+
+            static void Palindrome ()
+            {
+            int number;
+           
+            while (true)
+            {
+                try
+                {
+                    Console.Write("Укажите пятизначное число: ");
+                    number = Convert.ToInt32(Console.ReadLine());
+
+                    if(number >9999 && number < 100000)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ошибка ввода. Введите корректное число.");
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Ошибка ввода. Введите корректное число.");
+                }
+            }
+
+            if (((number % 10) == (number / 10000)) && (((number / 10) % 10) == ((number / 1000) % 10)))
+            {
+                Console.WriteLine($"Число {number} является палиндромом."); 
+            }
+            else
+            {
+                Console.WriteLine($"Число {number} не является палиндромом."); 
+            }
+            }
+            Palindrome ();
+        }
+    }
+}
---
+++ home_Seminar_3/Home_21/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Home3
+{
+    class TestMetods
+    {
+        public static void Main(string[] args)
+        {
+            // Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между
+            // ними в 3D пространстве.
+            // A (3,6,8); B (2,1,-7), -> 15.84
+            // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+            static void Distance ()
+            {
+              Console.WriteLine("Введите x1 для точки А: ");
+              int x1 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Введите y1 для точки А: ");
+              int y1 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Введите z1 для точки А: ");
+              int z1 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Введите x2 для точки B: ");
+              int x2 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Введите y2 для точки B: ");
+              int y2 = int.Parse(Console.ReadLine());
+
+              Console.WriteLine("Введите z2 для точки B: ");
+              int z2 = int.Parse(Console.ReadLine());
+
+              double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));
+              Console.WriteLine($"Расстояние между A ({x1},{y1},{z1}) и B ({x2},{y2};{z2}) -> {distance}");  
+            }
+            Distance ();
+        }
+    }
+}
