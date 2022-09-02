52eb0d497ec482539dd1045dd88468f6367e9f64
alex <shura888@inbox.ru>
2022-09-02 18:25:32 +0300

    Home_4-1

--- Home_Seminar_4/Task_25/Program.cs
+++ Home_Seminar_4/Task_25/Program.cs
@@ -5,7 +5,7 @@
 Console.WriteLine("Введите число А: ");
 int A = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Введите число А: ");
+Console.WriteLine("Введите число B: ");
 int B = int.Parse(Console.ReadLine());
 
 int result = 1;
